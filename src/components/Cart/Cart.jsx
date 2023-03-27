481551a7719ce1218675eaddcb9c3515b8d93938
alaminrifat <alaminrifat71@gmail.com>
2023-03-28 01:22:06 +0600

    local storage added and calculation done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,16 +5,19 @@ const Cart = ({ cart }) => {
     // console.log(props.cart);
     let total = 0;
     let totalShipping = 0 ;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price;
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
         totalShipping += product.shipping;
+        quantity  += product.quantity;
     }
     const tax = (total * 7) / 100;
     const grandTotal = total+totalShipping+tax;
     return (
         <div className="cart">
             <h4>Order Summery</h4>
-            <p>Selected Item: {cart.length}</p>
+            <p>Selected Item: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../../public/fakedb";
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -13,9 +14,28 @@ const Shop = () => {
             .then((resp) => resp.json())
             .then((data) => setProducts(data));
     }, []);
+
+    useEffect(() =>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // get id
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    },[products]) 
+    //load this useEffect again after the state of products changes
+
+
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     };
     return (
         <div className="shop-container">
